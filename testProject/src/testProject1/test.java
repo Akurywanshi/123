fd02733e8c3e8e860d8b3b7a8f1ccd38d322f961
Anant <Anant@LAPTOP-79TIMF26>
2022-11-03 15:51:46 +0530

    test class update

--- testProject/src/testProject1/test.java
+++ testProject/src/testProject1/test.java
@@ -3,7 +3,7 @@ package testProject1;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("test123");
+
 		System.out.println("Welcome");
 		System.out.println("Welcome too");
 	}
